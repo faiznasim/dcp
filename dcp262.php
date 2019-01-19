78b81e7361960e1f733cb4ff85d19aa3e5694851
Faiz Nasim <faiznasim.aust@gmail.com>
2019-01-19 18:02:33 +0600

    modified code of dcp 254 (using array)

--- dcp262.php
+++ dcp262.php
@@ -8,18 +8,31 @@ for($i = 1; $i <= $t; $i++)
 {
     fscanf(STDIN, "%d\n", $n);
     $x = pow(2,$n) - 2;
-    $b = decbin($x);
-    $arr = str_split($b);
-    $size = sizeof($arr);
-    if ($size == 1)
+    $store = $x;
+    $c = 0;
+    while($store != 0 && $x > 3) 
     {
-        fprintf(STDOUT, "%d 00%d\n", $n, $arr[0]);
+        $arr[$c] = $store % 2;
+        $store = (int) $store/2;
+        $c++;
     }
-    else if($size == 2)
+    if ($x == 0)
     {
-        fprintf(STDOUT, "%d 0%d%d\n", $n, $arr[0], $arr[1]);
+        fprintf(STDOUT, "%d 000\n", $n);
+    }
+    else if($x == 1)
+    {
+        fprintf(STDOUT, "%d 001\n", $n);
+    }
+    else if($x == 2)
+    {
+        fprintf(STDOUT, "%d 010\n", $n);
+    } 
+    else if($x == 3)
+    {
+        fprintf(STDOUT, "%d 011\n", $n);
     }
     else
-        fprintf(STDOUT, "%d %d%d%d\n", $n, $arr[$size-3], $arr[$size-2], $arr[$size-1]);
+        fprintf(STDOUT, "%d %d%d%d\n", $n, $arr[2], $arr[1], $arr[0]);
 }
 
