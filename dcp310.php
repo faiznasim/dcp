6e31b20d98a10acf26355d3293b1a25f5c1b2eb7
Faiz Nasim <faiznasim.aust@gmail.com>
2019-01-22 22:06:55 +0600

    written code for 310

---
+++ dcp310.php
@@ -0,0 +1,36 @@
+<?php
+
+$stdin = fopen('php://stdin', 'r');
+$stdout = fopen('php://stdout', 'w');
+
+fscanf(STDIN, "%d\n", $n);
+for($i = 0; $i < $n; $i++)
+{
+    fscanf(STDIN, "%d ", $arr[$i]);
+}
+fscanf(STDIN, "%d %d\n", $x, $k);
+$q = $x; // for position
+$store = $arr[$x - 1];
+$count = 0;
+for ($p = 1; $p <= $k; $p++,$q++)
+{
+    if ($q == $n)
+        break;
+    if ($arr[$q] > $store)
+    {
+        $count++;
+    }
+}
+
+$q = $x - 2;
+for ($p = 1; $p <= $k; $p++,$q--)
+{
+    if ($q < 0)
+        break;
+    if ($arr[$q] > $store)
+    {
+        $count++;
+    }
+}
+
+fprintf(STDOUT,"%d\n", $count);
