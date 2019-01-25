4e6428759d52beeb14b46bfb041dee52fcba7f94
Faiz Nasim <faiznasim.aust@gmail.com>
2019-01-25 18:28:19 +0600

    written code for dcp4.php

---
+++ dcp4.php
@@ -0,0 +1,21 @@
+<?php
+
+$stdin = fopen('php://stdin', 'r');
+$stdout = fopen('php://stdout', 'w');
+while(!feof($stdin))
+{
+    fscanf(STDIN, "%d %d\n", $hrs, $n);
+    $sum = 0;
+    for ($i = 0; $i < $n; $i++)
+    {
+        fscanf(STDIN, "%d\n", $arr[$i]);
+        $sum += $arr[$i];
+    }
+    $res = $hrs / $sum;
+    $final_res = ceil($res);
+    if ($final_res == 1)
+        fprintf(STDOUT, "Project will finish within 1 day.\n");
+    else
+        fprintf(STDOUT, "Project will finish within %d days.\n", $final_res);
+
+}
